f97aec1b53f4f32b33fa54581aedeb7738ba2ea7
Amaan Mahammad <amul.mahammad@gmail.com>
2021-08-12 19:32:49 -0700

    Add initial Robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
+    public DeviceManager deviceManager;
+
+    public Drivetrain drivetrain;
+
+    public Arm arm;
+    public Flap flap;
+    public TrayTilt trayTilt;
+
+    public Robot(HardwareMap hardwareMap) {
+        deviceManager = new DeviceManager(hardwareMap);
+    }
+
+    public void init(boolean autoIsRunning) {
+        deviceManager.init(autoIsRunning);
+
+        // drivetrain is only used for TeleOp
+        if (!autoIsRunning) {
+            drivetrain = new Drivetrain(deviceManager);
+        }
+
+        arm = new Arm(deviceManager);
+        flap = new Flap(deviceManager);
+        trayTilt = new TrayTilt(deviceManager);
+    }
 }
