96ea9193ca0411bc50549049418335a2f429d53c
Yeshu1 <bansalyeshasv@gmail.com>
2021-08-12 19:08:34 -0700

    Flap Hardware Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Flap.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Flap {
+    public Servo flap;
+
+    public Position currentPosition;
+
+    public enum Position{
+        OPEN(0),//when flap lets blocks through
+        CLOSE(1);//when flap lets balls through
+
+        public double flapPosition;
+
+        Position(double flapPosition){
+            this.flapPosition = flapPosition;
+        }
+    }
+
+    public Flap (DeviceManager deviceManager){
+
+        flap = deviceManager.flap;
+
+        flap.setPosition(0);
+
+        flap.setDirection(Servo.Direction.FORWARD);
+
+    }
+
+    public void run(Position position){
+        flap.setPosition(position.flapPosition);
+
+        //updates current position
+        currentPosition = position;
+    }
+
+    public void open(){
+        run(Position.OPEN);//opens flap to let blocks through
+    }
+
+    public void close(){
+        run(Position.CLOSE);//closes flap to let balls through
+    }
+}
